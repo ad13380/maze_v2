2fe43568c8c78868f5989e8740149d658203a659
Anthony <a.a.donovan@outlook.com>
2020-09-28 18:48:24 +0100

    adds tests

---
+++ src/hooks/useClearVisitedNodes/useClearVisitedNodes.test.js
@@ -0,0 +1,60 @@
+import { useClearVisitedNodes } from "./useClearVisitedNodes";
+
+describe("useClearVisitedNodes", () => {
+  let grid;
+  let updatedGrid;
+  const [getClearVisitedNodes] = useClearVisitedNodes();
+  const visitedNodes = [
+    { row: 0, col: 0 },
+    { row: 1, col: 1 },
+    { row: 2, col: 2 },
+  ];
+
+  const generateGrid = (nodeType) => {
+    return new Array(3).fill("").map(() =>
+      new Array(3).fill("").map(() => ({
+        type: nodeType,
+      }))
+    );
+  };
+
+  const getNodeTypeTotal = (grid, nodeType) => {
+    let counter = 0;
+    grid.forEach((row) => {
+      row.forEach((node) => {
+        if (node.type === nodeType) counter++;
+      });
+    });
+    return counter;
+  };
+
+  it("returns original grid if there are no visited nodes", () => {
+    grid = generateGrid("wall");
+    updatedGrid = getClearVisitedNodes(grid, []);
+    expect(updatedGrid).toEqual(grid);
+  });
+
+  it("does not change node type for wall nodes", () => {
+    grid = generateGrid("wall");
+    updatedGrid = getClearVisitedNodes(grid, visitedNodes);
+    expect(getNodeTypeTotal(updatedGrid, "wall")).toEqual(9);
+  });
+
+  it("does not change node type for empty nodes", () => {
+    grid = generateGrid("");
+    updatedGrid = getClearVisitedNodes(grid, visitedNodes);
+    expect(getNodeTypeTotal(updatedGrid, "")).toEqual(9);
+  });
+
+  it("changes visited node type to empty node type", () => {
+    grid = generateGrid("visited");
+    updatedGrid = getClearVisitedNodes(grid, visitedNodes);
+    expect(getNodeTypeTotal(updatedGrid, "visited")).toEqual(6);
+  });
+
+  it("changes path node type to empty node type", () => {
+    grid = generateGrid("path");
+    updatedGrid = getClearVisitedNodes(grid, visitedNodes);
+    expect(getNodeTypeTotal(updatedGrid, "path")).toEqual(6);
+  });
+});
