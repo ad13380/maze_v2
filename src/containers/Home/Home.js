51ac645a7d4b4cbb226bf4d6b203f23425fd53da
Anthony <a.a.donovan@outlook.com>
2020-10-20 21:29:57 +0100

    Adds styling to Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <Link to="/visualise">
-        <button data-test="visualise-page-button">Visualise</button>
-      </Link>
-      <br />
-      <Link to="/tutorial">
-        <button data-test="tutorial-page-button">How To Use</button>
-      </Link>
-      <br />
-      <a href="https://github.com/ad13380/">Github: Anthony Donovan</a>
+      <div className="home-wrapper">
+        <h3>An Interactive Pathfinding Algorithm Visualiser</h3>
+        <Link to="/visualise">
+          <button className="neomorphic" data-test="visualise-page-button">
+            Visualise
+          </button>
+        </Link>
+        <Link to="/tutorial">
+          <button className="neomorphic" data-test="tutorial-page-button">
+            How To Use
+          </button>
+        </Link>
+        <a id="github-link" href="https://github.com/ad13380/">
+          Github: Anthony Donovan
+        </a>
+      </div>
     </>
   );
 };
