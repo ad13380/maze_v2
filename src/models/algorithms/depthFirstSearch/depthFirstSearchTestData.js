37056fb41b5ddd1a98ff3232aeb75a959188668f
Anthony <a.a.donovan@outlook.com>
2020-10-21 18:04:04 +0100

    All tests passing

---
+++ src/models/algorithms/depthFirstSearch/depthFirstSearchTestData.js
@@ -0,0 +1,31 @@
+export const expectedDFSResult = [
+  { row: 3, col: 2 },
+  { row: 3, col: 1 },
+  { row: 3, col: 0 },
+  { row: 4, col: 0 },
+  { row: 4, col: 1 },
+  { row: 4, col: 2 },
+  { row: 5, col: 2 },
+  { row: 5, col: 3 },
+  { row: 5, col: 4 },
+  { row: 5, col: 5 },
+  { row: 4, col: 5 },
+  { row: 4, col: 4 },
+  { row: 3, col: 4 },
+];
+
+export const expectedPathResult = [
+  { row: 3, col: 2 },
+  { row: 3, col: 1 },
+  { row: 3, col: 0 },
+  { row: 4, col: 0 },
+  { row: 4, col: 1 },
+  { row: 4, col: 2 },
+  { row: 5, col: 2 },
+  { row: 5, col: 3 },
+  { row: 5, col: 4 },
+  { row: 5, col: 5 },
+  { row: 4, col: 5 },
+  { row: 4, col: 4 },
+  { row: 3, col: 4 },
+];
